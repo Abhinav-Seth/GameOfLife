c507b6b8ca49a324b8b11ea7496dad5d8e1f8f07
Abhinav-Seth <abhinav.seth@swiggy.in>
2023-03-06 17:23:33 +0530

    intial implementation

---
+++ src/github.com/abhinav/app/Main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("input row cols ticks respectively:")
+	var M, N, ticks int
+	_, err := fmt.Scan(&M, &N, &ticks)
+	if err != nil {
+		log.Fatal(err)
+	}
+	initialGrid := intialiseGrid(M, N)
+	fmt.Println("intial Grid")
+	printGrid(initialGrid)
+	for i := 0; i < ticks; i++ {
+		nextGrid := nextGeneration(initialGrid, M, N)
+		fmt.Printf("After %d tick(s): \n", i+1)
+		printGrid(nextGrid)
+		initialGrid = nextGrid
+	}
+
+}
+
+func intialiseGrid(M int, N int) [][]int {
+	grid := getArray(M, N)
+	for k := 0; k < rand.Intn(M*N); k++ {
+		i := rand.Intn(M)
+		j := rand.Intn(N)
+		grid[i][j] = 1
+	}
+	return grid
+}
+
+func getArray(M int, N int) [][]int {
+	grid := make([][]int, M)
+	for i := range grid {
+		grid[i] = make([]int, N)
+	}
+	return grid
+}
+
+func nextGeneration(intialGrid [][]int, M int, N int) [][]int {
+	finalGrid := getArray(M, N)
+	for i := range intialGrid {
+		for j := range intialGrid {
+			isLive := intialGrid[i][j] == 1
+			liveN := countLiveNeighbors(intialGrid, i, j, M, N)
+			if isLive && liveN == 2 || liveN == 3 {
+				finalGrid[i][j] = 1
+			} else if !isLive && liveN == 3 {
+				finalGrid[i][j] = 1
+			} else {
+				finalGrid[i][j] = 0
+			}
+		}
+	}
+	return finalGrid
+}
+
+func countLiveNeighbors(intialGrid [][]int, i int, j, M int, N int) int {
+	countLive := 0
+	if i < M-1 && intialGrid[i+1][j] == 1 {
+		countLive++
+	}
+	if j < N-1 && intialGrid[i][j+1] == 1 {
+		countLive++
+	}
+	if i < M-1 && j < N-1 && intialGrid[i+1][j+1] == 1 {
+		countLive++
+	}
+	if i > 0 && intialGrid[i-1][j] == 1 {
+		countLive++
+	}
+	if j > 0 && intialGrid[i][j-1] == 1 {
+		countLive++
+	}
+	if i > 0 && j > 0 && intialGrid[i-1][j-1] == 1 {
+		countLive++
+	}
+	return countLive
+}
+
+func printGrid(grid [][]int) {
+	for i := range grid {
+		fmt.Println(grid[i])
+	}
+}
